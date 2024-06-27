0368e7052c128df28b46416dc759d82b59a46444
Wosmerik Alexander <shurik1777@gmail.com>
2024-06-27 10:25:43 +0300

    remove_category_photo

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -11,15 +11,15 @@ admin.site.index_title = 'Администрирование сайта'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'photo')
+    list_display = ('name', 'description')
     list_display_links = ('name',)
     search_fields = ('name', 'description')
     # readonly_fields = ('photo',)  # - Чтобы сделать поле "photo" не редактируемым в админке, раскомментировать строку.
-
-    def photo(self, obj):
-        if obj.photo:
-            return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.photo.url))
-        else:
-            return 'No Photo'
-
-    photo.allow_tags = True
+    #
+    # def photo(self, obj):
+    #     if obj.photo:
+    #         return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.photo.url))
+    #     else:
+    #         return 'No Photo'
+    #
+    # photo.allow_tags = True
--- cocktail/models.py
+++ cocktail/models.py
@@ -4,10 +4,10 @@ from django.db import models as m
 class Category(m.Model):
     name = m.CharField(max_length=100)
     description = m.TextField(default='', blank=True)
-    photo = m.ImageField(upload_to='category_photos', blank=True, null=True)
+    # photo = m.ImageField(upload_to='category_photos', blank=True, null=True)
 
     def __str__(self):
-        return f"Категория: {self.name}, Описание: {self.description}, Фото: {self.photo}"
+        return f"Категория: {self.name}, Описание: {self.description}"
 
     class Meta:
         verbose_name = "Категория"
