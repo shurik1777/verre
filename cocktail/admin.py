05e56732b96aa9f56b130852e3f3fbc80dde802b
Wosmerik Alexander <shurik1777@gmail.com>
2024-06-27 10:09:43 +0300

    add Category add work in admins

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'photo')
+    list_display_links = ('name',)
+    search_fields = ('name', 'description')
+    # readonly_fields = ('photo',)  # - Чтобы сделать поле "photo" редактируемым в админке, удалите его из readonly_fields.
+
+    def photo(self, obj):
+        if obj.photo:
+            return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.photo.url))
+        else:
+            return 'No Photo'
+
+    photo.allow_tags = True
--- cocktail/models.py
+++ cocktail/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class Category(m.Model):
+    name = m.CharField(max_length=100)
+    description = m.TextField(default='', blank=True)
+    photo = m.ImageField(upload_to='category_photos', blank=True, null=True)
+
+    def __str__(self):
+        return f"Категория: {self.name}, Описание: {self.description}, Фото: {self.photo}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
