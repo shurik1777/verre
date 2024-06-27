6fb88a8ebb08abbdb3423fed4dcb8758eb6b112a
Wosmerik Alexander <shurik1777@gmail.com>
2024-06-27 14:43:46 +0300

    add recipe and coherence

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Category, Ingredient
+from .models import Category, Ingredient, Unit, Recipe, RecipeIngredient
 
 admin.site.site_header = 'Verre - сайт рецептов алкогольных напитков'
 admin.site.site_title = 'Администрирование сайта'
@@ -11,9 +11,15 @@ admin.site.index_title = 'Администрирование сайта'
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-    list_display = ('name', 'description')
+    list_display = ('name', 'description', "slug")
     list_display_links = ('name',)
-    search_fields = ('name', 'description')
+    list_editable = ["slug", ]
+    search_fields = ('name', 'description', "slug")
+    fields = [
+        "name",
+        "slug",
+        "description",
+    ]
 
 
 @admin.register(Ingredient)
@@ -23,6 +29,7 @@ class IngredientAdmin(admin.ModelAdmin):
     list_editable = ["description", ]
     search_fields = ["name", "description"]
     list_filter = ["name", "description"]
+
     # readonly_fields = ('image',)  # - Чтобы сделать поле "image" не редактируемым в админке, раскомментировать строку.
 
     def image(self, obj):
@@ -39,3 +46,60 @@ class IngredientAdmin(admin.ModelAdmin):
         "image",
         "category",
     ]
+
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('u_name', 'u_anno')
+    list_editable = ['u_anno', ]
+    # list_display_links = ('u_name',)
+    search_fields = ('u_anno',)
+    fields = [
+        "u_name",
+        "u_anno",
+    ]
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'author', 'preparation_time', 'image']
+    list_display_links = ('title',)
+    search_fields = ('title', 'description', 'author')
+    readonly_fields = ('image',)
+    fields = [
+        "title",
+        "slug",
+        "description",
+        "preparation_steps",
+        "preparation_time",
+        "image",
+        "author",
+    ]
+
+    def image(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.image.url))
+        else:
+            return 'No image'
+
+    image.allow_tags = True
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'unit', 'register_date')
+    list_filter = ('recipe',)
+    search_fields = ('ingredient__name',)
+    fields = [
+        "recipe",
+        "ingredient",
+        "unit",
+    ]
+
+    # Метод для отображения категории рецепта в форме редактирования
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "recipe":
+            kwargs["queryset"] = Recipe.objects.all().order_by('title')
+            return super().formfield_for_foreignkey(db_field, request, **kwargs)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
--- cocktail/models.py
+++ cocktail/models.py
@@ -27,14 +27,55 @@ class Ingredient(m.Model):
     def __str__(self):
         return f"{self.name}, цена: {self.description}"
 
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
+    def display_id(self):
+        return f"{self.id:05}"
+
     class Meta:
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
         ordering = ("id",)
 
-    def get_absolute_url(self):
-        return reverse("catalog:product", kwargs={"product_slug": self.slug})
 
-    def display_id(self):
-        return f"{self.id:05}"
+class Unit(m.Model):
+    u_name = m.CharField(max_length=16, null=False, db_index=True, unique=True,
+                         verbose_name='Обозначение единицы измерения')
+    u_anno = m.CharField(max_length=128, null=False, verbose_name='Краткое описание')
+
+    def __str__(self):
+        return f'{self.u_anno} ({self.u_name})'
+
+    class Meta:
+        verbose_name = 'Единица измерения'
+        verbose_name_plural = 'Единицы измерения'
+
+
+class Recipe(m.Model):
+    title = m.CharField(max_length=128, unique=True, verbose_name='Название')
+    slug = m.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = m.TextField(verbose_name='Описание')
+    preparation_steps = m.TextField(verbose_name='Шаги приготовления')
+    preparation_time = m.PositiveIntegerField(verbose_name='Время приготовления (в минутах)')
+    image = m.ImageField(upload_to='recipe_images', blank=True, null=True, verbose_name='Изображение')
+    author = m.CharField(max_length=128, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+class RecipeIngredient(m.Model):
+    recipe = m.ForeignKey(Recipe, on_delete=m.CASCADE, related_name='ingredients', verbose_name='Рецепт')
+    ingredient = m.ForeignKey(Ingredient, on_delete=m.CASCADE, verbose_name='Ингредиент')
+    unit = m.ForeignKey(Unit, on_delete=m.CASCADE, verbose_name='Единица измерения')
+    register_date = m.DateField(auto_now_add=True, verbose_name='Дата создания рецепта')
+
+    class Meta:
+        verbose_name = 'Рецепт Коктейля'
+        verbose_name_plural = 'Рецепты Коктейлей'
