257f3af45b24962745a9777879dd1b3d94b7ab34
Wosmerik Alexander <shurik1777@gmail.com>
2024-06-27 11:22:07 +0300

    003 migrate refactoring Category, add Ingredient

--- cocktail/admin.py
+++ cocktail/admin.py
@@ -1,25 +1,41 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Category
+from .models import Category, Ingredient
 
 admin.site.site_header = 'Verre - сайт рецептов алкогольных напитков'
 admin.site.site_title = 'Администрирование сайта'
 admin.site.index_title = 'Администрирование сайта'
 
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
     list_display = ('name', 'description')
     list_display_links = ('name',)
     search_fields = ('name', 'description')
-    # readonly_fields = ('photo',)  # - Чтобы сделать поле "photo" не редактируемым в админке, раскомментировать строку.
-    #
-    # def photo(self, obj):
-    #     if obj.photo:
-    #         return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.photo.url))
-    #     else:
-    #         return 'No Photo'
-    #
-    # photo.allow_tags = True
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "description", "image"]
+    list_editable = ["description", ]
+    search_fields = ["name", "description"]
+    list_filter = ["name", "description"]
+    # readonly_fields = ('image',)  # - Чтобы сделать поле "image" не редактируемым в админке, раскомментировать строку.
+
+    def image(self, obj):
+        if obj.image:
+            return mark_safe('<img src="{}" width="100" height="100" />'.format(obj.photo.url))
+        else:
+            return 'No image'
+
+    image.allow_tags = True
+    fields = [
+        "name",
+        "slug",
+        "description",
+        "image",
+        "category",
+    ]
--- cocktail/models.py
+++ cocktail/models.py
@@ -1,14 +1,40 @@
 from django.db import models as m
+from django.urls import reverse
 
 
 class Category(m.Model):
-    name = m.CharField(max_length=100)
-    description = m.TextField(default='', blank=True)
-    # photo = m.ImageField(upload_to='category_photos', blank=True, null=True)
+    name = m.CharField(max_length=150, unique=True, verbose_name='Название')
+    description = m.TextField(blank=True, null=True, verbose_name='Описание')
+    slug = m.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     def __str__(self):
-        return f"Категория: {self.name}, Описание: {self.description}"
+        return f"Категория: {self.name}, Описание: {self.description} slug{self.slug}"
 
     class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ("id",)
+
+
+class Ingredient(m.Model):
+    name = m.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug = m.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = m.TextField(blank=True, null=True, verbose_name='Описание')
+    image = m.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    category = m.ForeignKey(to=Category, on_delete=m.CASCADE, verbose_name='Категория')
+
+    def __str__(self):
+        return f"{self.name}, цена: {self.description}"
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ("id",)
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
+    def display_id(self):
+        return f"{self.id:05}"
